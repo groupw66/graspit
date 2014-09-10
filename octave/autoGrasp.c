43285bc03809153721af255ab8b24308e0bec816
groupdoll <GroupDoll>
2014-09-10 15:54:16 +0700

    add autoGrasp octave interface

---
+++ octave/autoGrasp.c
@@ -0,0 +1,38 @@
+#include "connectToServer.h"
+/*#include <stdio.h>
+#ifdef WIN32
+#include <winsock2.h>
+#else
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <errno.h>
+#endif
+*/
+#define MAXLENGTH 500
+
+#include "mex.h"
+
+void mexFunction(int nlhs,       mxArray *plhs[],
+                 int nrhs, const mxArray *prhs[])
+{
+  int sockd;
+
+  /* Check for proper number of arguments */
+
+  if (nrhs != 0) {
+    mexErrMsgTxt("autoGrasp takes no arguments.");
+  } else if (nlhs != 0) {
+    mexErrMsgTxt("autoGrasp takes no output arguments.");
+  }
+
+  sockd = ConnectTo("localhost",4765);
+
+  if (sockd < 0)
+    mexErrMsgTxt("Could not connect");
+
+  Writeline(sockd,"autoGrasp\n",10);
+
+  CloseConnection(sockd);
+  return;
+}
