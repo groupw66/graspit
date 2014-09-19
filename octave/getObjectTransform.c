8e35f94de584dda549b4578bd0937a45429106c7
Mana <mana_ohm@hotmail.com>
2014-09-19 13:33:47 +0700

    make transform capable of output in separate quaternion and translation

--- octave/getObjectTransform.c
+++ octave/getObjectTransform.c
@@ -14,80 +14,95 @@
 #include "mex.h"
 
 void mexFunction(int nlhs,       mxArray *plhs[],
-                 int nrhs, const mxArray *prhs[])
+								 int nrhs, const mxArray *prhs[])
 {
-  char buf[MAXLENGTH];
-  char numStr[20];
-  int i,j,numBodies;
-  double *pvals,*pind;
-  int sockd;
-  mxArray *vals;
+	char buf[MAXLENGTH];
+	char numStr[20];
+	int i,j,numRobots;
+	double *pvals,*pvals2,*pind;
+	int sockd;
+	mxArray *vals,*vals2;
 
-  /* Check for proper number of arguments */
+	/* Check for proper number of arguments */
 
-  if (nrhs > 1) {
-    mexErrMsgTxt("getObjectTransform takes at most one input argument.");
-  } else if (nlhs > 1) {
-    mexErrMsgTxt("getObjectTransform takes at most one output argument.");
-  }
-
-  sockd = ConnectTo("localhost",4765);
-
-  if (sockd < 0)
-    mexErrMsgTxt("Could not connect");
-
-  strcpy(buf,"getObjectTransform ");
-
-  if (nrhs==0) {
-	strcat(buf,"ALL\n");
-	Writeline(sockd,buf,strlen(buf));
-
-    Readline(sockd,buf,MAXLENGTH);
-    sscanf(buf,"%d\n",&numBodies);
-  }
-  else {
-	  numBodies = mxGetNumberOfElements(prhs[0]);
-	  if (numBodies>0) {
-	  sprintf(numStr,"%d ",numBodies);
-	  strcat(buf,numStr);
-
-	  pind = mxGetPr(prhs[0]);
-	  for (i=0;i<numBodies-1;i++) {
-	    sprintf(numStr,"%d ",(int)pind[i]-1);
-  		strcat(buf,numStr);
-	  }
-	  sprintf(numStr,"%d\n",(int)pind[i]-1);
-	  strcat(buf,numStr);
-	  Writeline(sockd,buf,strlen(buf));
+	if (nrhs > 1) {
+		mexErrMsgTxt("getObjectTransform takes at most one input argument.");
+	} else if (nlhs > 2) {
+		mexErrMsgTxt("getObjectTransform takes at most two output argument.");
 	}
-  }
 
-  if (numBodies == 0) {
-	plhs[0] = NULL;
-  }
-  else {
-    plhs[0] = mxCreateCellArray(1, &numBodies);
+	sockd = ConnectTo("localhost",4765);
 
-		if (numBodies > 1){
+	if (sockd < 0)
+		mexErrMsgTxt("Could not connect");
+
+	strcpy(buf,"getObjectTransform ");
+
+	if (nrhs==0) {
+		strcat(buf,"ALL\n");
+		Writeline(sockd,buf,strlen(buf));
+
+		Readline(sockd,buf,MAXLENGTH);
+		sscanf(buf,"%d\n",&numRobots);
+	}
+	else {
+		numRobots = mxGetNumberOfElements(prhs[0]);
+		if (numRobots>0) {
+			sprintf(numStr,"%d ",numRobots);
+			strcat(buf,numStr);
+
+			pind = mxGetPr(prhs[0]);
+			for (i=0;i<numRobots-1;i++) {
+				sprintf(numStr,"%d ",(int)pind[i]-1);
+				strcat(buf,numStr);
+			}
+			sprintf(numStr,"%d\n",(int)pind[i]-1);
+			strcat(buf,numStr);
+			Writeline(sockd,buf,strlen(buf));
+		}
+	}
+
+	if (numRobots == 0) {
+		plhs[0] = NULL;
+	}
+	else {
+
+		if (numRobots > 1){
 			for (i=0;i<nlhs;i++)
-				plhs[i] = mxCreateCellArray(1,&numBodies);
+				plhs[i] = mxCreateCellArray(1,&numRobots);
 		}
 
-		for (i=0;i<numBodies;i++) {
-
-			vals = mxCreateDoubleMatrix(7,1,mxREAL);
+		for (i=0;i<numRobots;i++) {
+			if(nlhs==1)
+				vals = mxCreateDoubleMatrix(7,1,mxREAL);
+			else{
+				vals = mxCreateDoubleMatrix(4,1,mxREAL);
+				vals2 = mxCreateDoubleMatrix(3,1,mxREAL);
+				pvals2 = mxGetPr(vals2);
+			}
 			pvals = mxGetPr(vals);
 
 			Readline(sockd,buf,MAXLENGTH);
-			sscanf(buf,"%lf%lf%lf%lf%lf%lf%lf",&pvals[0],&pvals[1],&pvals[2],&pvals[3],&pvals[4],&pvals[5],&pvals[6]);
-			if (numBodies == 1) {
-				plhs[0] = vals;
-			} else {
-				mxSetCell(plhs[0],i,vals);
+			if(nlhs==1){
+				sscanf(buf,"%lf%lf%lf%lf%lf%lf%lf",&pvals[0],&pvals[1],&pvals[2],&pvals[3],&pvals[4],&pvals[5],&pvals[6]);
+				if (numRobots == 1)
+					plhs[0] = vals;
+				else
+					mxSetCell(plhs[0],i,vals);
+			}
+			else{
+				sscanf(buf,"%lf%lf%lf%lf%lf%lf%lf",&pvals[0],&pvals[1],&pvals[2],&pvals[3],&pvals2[0],&pvals2[1],&pvals2[2]);
+				if (numRobots == 1) {
+					plhs[0] = vals;
+					plhs[1] = vals2;
+				} else {
+					mxSetCell(plhs[0],i,vals);
+					mxSetCell(plhs[1],i,vals2);
+				}
 			}
 		}
-  }
+	}
 
-  CloseConnection(sockd);
-  return;
+	CloseConnection(sockd);
+	return;
 }
--- octave/getRobotTransform.c
+++ octave/getRobotTransform.c
@@ -14,59 +14,58 @@
 #include "mex.h"
 
 void mexFunction(int nlhs,       mxArray *plhs[],
-                 int nrhs, const mxArray *prhs[])
+								 int nrhs, const mxArray *prhs[])
 {
-  char buf[MAXLENGTH];
-  char numStr[20];
-  int i,j,numRobots;
-  double *pvals,*pind;
-  int sockd;
-  mxArray *vals;
+	char buf[MAXLENGTH];
+	char numStr[20];
+	int i,j,numRobots;
+	double *pvals,*pvals2,*pind;
+	int sockd;
+	mxArray *vals,*vals2;
 
-  /* Check for proper number of arguments */
+	/* Check for proper number of arguments */
 
-  if (nrhs > 1) {
-    mexErrMsgTxt("getRobotTransform takes at most one input argument.");
-  } else if (nlhs > 1) {
-    mexErrMsgTxt("getRobotTransform takes at most one output argument.");
-  }
-
-  sockd = ConnectTo("localhost",4765);
-
-  if (sockd < 0)
-    mexErrMsgTxt("Could not connect");
-
-  strcpy(buf,"getRobotTransform ");
-
-  if (nrhs==0) {
-	strcat(buf,"ALL\n");
-	Writeline(sockd,buf,strlen(buf));
-
-    Readline(sockd,buf,MAXLENGTH);
-    sscanf(buf,"%d\n",&numRobots);
-  }
-  else {
-	  numRobots = mxGetNumberOfElements(prhs[0]);
-	  if (numRobots>0) {
-	  sprintf(numStr,"%d ",numRobots);
-	  strcat(buf,numStr);
-
-	  pind = mxGetPr(prhs[0]);
-	  for (i=0;i<numRobots-1;i++) {
-	    sprintf(numStr,"%d ",(int)pind[i]-1);
-  		strcat(buf,numStr);
-	  }
-	  sprintf(numStr,"%d\n",(int)pind[i]-1);
-	  strcat(buf,numStr);
-	  Writeline(sockd,buf,strlen(buf));
+	if (nrhs > 1) {
+		mexErrMsgTxt("getRobotTransform takes at most one input argument.");
+	} else if (nlhs > 2) {
+		mexErrMsgTxt("getRobotTransform takes at most two output argument.");
 	}
-  }
 
-  if (numRobots == 0) {
-	plhs[0] = NULL;
-  }
-  else {
-    plhs[0] = mxCreateCellArray(1, &numRobots);
+	sockd = ConnectTo("localhost",4765);
+
+	if (sockd < 0)
+		mexErrMsgTxt("Could not connect");
+
+	strcpy(buf,"getRobotTransform ");
+
+	if (nrhs==0) {
+		strcat(buf,"ALL\n");
+		Writeline(sockd,buf,strlen(buf));
+
+		Readline(sockd,buf,MAXLENGTH);
+		sscanf(buf,"%d\n",&numRobots);
+	}
+	else {
+		numRobots = mxGetNumberOfElements(prhs[0]);
+		if (numRobots>0) {
+			sprintf(numStr,"%d ",numRobots);
+			strcat(buf,numStr);
+
+			pind = mxGetPr(prhs[0]);
+			for (i=0;i<numRobots-1;i++) {
+				sprintf(numStr,"%d ",(int)pind[i]-1);
+				strcat(buf,numStr);
+			}
+			sprintf(numStr,"%d\n",(int)pind[i]-1);
+			strcat(buf,numStr);
+			Writeline(sockd,buf,strlen(buf));
+		}
+	}
+
+	if (numRobots == 0) {
+		plhs[0] = NULL;
+	}
+	else {
 
 		if (numRobots > 1){
 			for (i=0;i<nlhs;i++)
@@ -74,20 +73,36 @@ void mexFunction(int nlhs,       mxArray *plhs[],
 		}
 
 		for (i=0;i<numRobots;i++) {
-
-			vals = mxCreateDoubleMatrix(7,1,mxREAL);
+			if(nlhs==1)
+				vals = mxCreateDoubleMatrix(7,1,mxREAL);
+			else{
+				vals = mxCreateDoubleMatrix(4,1,mxREAL);
+				vals2 = mxCreateDoubleMatrix(3,1,mxREAL);
+				pvals2 = mxGetPr(vals2);
+			}
 			pvals = mxGetPr(vals);
 
 			Readline(sockd,buf,MAXLENGTH);
-			sscanf(buf,"%lf%lf%lf%lf%lf%lf%lf",&pvals[0],&pvals[1],&pvals[2],&pvals[3],&pvals[4],&pvals[5],&pvals[6]);
-			if (numRobots == 1) {
-				plhs[0] = vals;
-			} else {
-				mxSetCell(plhs[0],i,vals);
+			if(nlhs==1){
+				sscanf(buf,"%lf%lf%lf%lf%lf%lf%lf",&pvals[0],&pvals[1],&pvals[2],&pvals[3],&pvals[4],&pvals[5],&pvals[6]);
+				if (numRobots == 1)
+					plhs[0] = vals;
+				else
+					mxSetCell(plhs[0],i,vals);
+			}
+			else{
+				sscanf(buf,"%lf%lf%lf%lf%lf%lf%lf",&pvals[0],&pvals[1],&pvals[2],&pvals[3],&pvals2[0],&pvals2[1],&pvals2[2]);
+				if (numRobots == 1) {
+					plhs[0] = vals;
+					plhs[1] = vals2;
+				} else {
+					mxSetCell(plhs[0],i,vals);
+					mxSetCell(plhs[1],i,vals2);
+				}
 			}
 		}
-  }
+	}
 
-  CloseConnection(sockd);
-  return;
+	CloseConnection(sockd);
+	return;
 }
