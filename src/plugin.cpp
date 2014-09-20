b1b406a610be9598e75532b945b85e27b2612b89
Mana <mana_ohm@hotmail.com>
2014-09-20 12:12:18 +0700

    change default pluginDirs to be plugins folder relative to GRASPIT environment variable

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -41,101 +41,102 @@ extern "C"{
 
 PluginCreator::~PluginCreator()
 {
-  dlclose(mLibraryHandle);
+	dlclose(mLibraryHandle);
 }
 
 Plugin* PluginCreator::createPlugin(std::string args)
-{	
-  Plugin* plugin = (*mCreatePluginFctn)(); 
-  args = args;
-  if (!plugin) return NULL;
-  if (plugin->init(0,NULL) != SUCCESS) {
-    DBGA("Failed to initialize new plugin of type " << mType);
-    delete plugin;
-    return NULL;
-  }
-  return plugin;
+{
+	Plugin* plugin = (*mCreatePluginFctn)();
+	args = args;
+	if (!plugin) return NULL;
+	if (plugin->init(0,NULL) != SUCCESS) {
+		DBGA("Failed to initialize new plugin of type " << mType);
+		delete plugin;
+		return NULL;
+	}
+	return plugin;
 }
 
 PluginCreator* PluginCreator::loadFromLibrary(std::string libName)
 {
-  QString filename = QString::fromStdString(libName);
-  //append library suffix if it does not already have it
-  if (!filename.endsWith(LIBRARY_SUFFIX)) {
-    filename.append(LIBRARY_SUFFIX);
-  }
+	QString filename = QString::fromStdString(libName);
+	//append library suffix if it does not already have it
+	if (!filename.endsWith(LIBRARY_SUFFIX)) {
+		filename.append(LIBRARY_SUFFIX);
+	}
 
-  if (filename.startsWith("/")) {
-    //filename is an absolute path
-    QFile file(filename);
-    if (!file.exists()) {
-      DBGA("Could not find absolute plugin file " << filename.latin1());
-      return NULL;
-    }
-  } else{
-    //filename is relative to GRASPIT_PLUGIN_DIR
-    QString pluginDirs = QString(getenv("GRASPIT_PLUGIN_DIR"));
-    if (pluginDirs.isNull()) {
-      DBGA("Relative plugin file specified, but GRASPIT_PLUGIN_DIR is not set");
-      return NULL;
-    }
-    bool found = false;
-    for (int i=0; i<=pluginDirs.count(","); i++) {
-      QString dir = pluginDirs.section(',',i,i);
-      if (!dir.endsWith("/")) dir.append("/");
-      dir.append(filename);
-      QFile file(dir);
-      if (file.exists()) {
-        found = true;
-        filename = dir;
-        break;
-      }
-    }
-    if (!found) {
-      DBGA("Could not find relative plugin file " << filename.latin1() << 
-           " in any directory specified in GRASPIT_PLUGIN_DIR");
-      return NULL;
-    }    
-  }
+	if (filename.startsWith("/")) {
+		//filename is an absolute path
+		QFile file(filename);
+		if (!file.exists()) {
+			DBGA("Could not find absolute plugin file " << filename.latin1());
+			return NULL;
+		}
+	} else{
+		//filename is relative to GRASPIT_PLUGIN_DIR
+		//QString pluginDirs = QString(getenv("GRASPIT_PLUGIN_DIR"));
+		QString pluginDirs = QString(getenv("GRASPIT"))+QString("/plugins");
+		if (pluginDirs.isNull()) {
+			DBGA("Relative plugin file specified, but GRASPIT_PLUGIN_DIR is not set");
+			return NULL;
+		}
+		bool found = false;
+		for (int i=0; i<=pluginDirs.count(","); i++) {
+			QString dir = pluginDirs.section(',',i,i);
+			if (!dir.endsWith("/")) dir.append("/");
+			dir.append(filename);
+			QFile file(dir);
+			if (file.exists()) {
+				found = true;
+				filename = dir;
+				break;
+			}
+		}
+		if (!found) {
+			DBGA("Could not find relative plugin file " << filename.latin1() <<
+					 " in any directory specified in GRASPIT_PLUGIN_DIR");
+			return NULL;
+		}
+	}
 
-  //look for the library file and load it
-   void* handle = dlopen(filename.toAscii().constData(), RTLD_NOW | RTLD_GLOBAL);
-  char *errstr = dlerror();
-  if (!handle) {
-    DBGA("Failed to open dynamic library " << filename.toAscii().constData() );
-    if (errstr) DBGA("Error: " << errstr);
-    return NULL;
-  }
+	//look for the library file and load it
+	 void* handle = dlopen(filename.toAscii().constData(), RTLD_NOW | RTLD_GLOBAL);
+	char *errstr = dlerror();
+	if (!handle) {
+		DBGA("Failed to open dynamic library " << filename.toAscii().constData() );
+		if (errstr) DBGA("Error: " << errstr);
+		return NULL;
+	}
 
-  //instantiate the plugin inside of the library
-  //this is pretty bad, but is based on example here:
-  // http://pubs.opengroup.org/onlinepubs/009695399/functions/dlsym.html
-  //see also discussion here:
-  // http://www.trilithium.com/johan/2004/12/problem-with-dlsym/
-  //maybe in the future a better solution can be found...
-  PluginCreator::CreatePluginFctn createPluginFctn;
-  *(void **)(&createPluginFctn) = dlsym(handle,"createPlugin");
-  if (dlerror()) {
-    DBGA("Could not load symbol createPlugin from library " << filename.toAscii().constData());
-    return NULL;
-  }
+	//instantiate the plugin inside of the library
+	//this is pretty bad, but is based on example here:
+	// http://pubs.opengroup.org/onlinepubs/009695399/functions/dlsym.html
+	//see also discussion here:
+	// http://www.trilithium.com/johan/2004/12/problem-with-dlsym/
+	//maybe in the future a better solution can be found...
+	PluginCreator::CreatePluginFctn createPluginFctn;
+	*(void **)(&createPluginFctn) = dlsym(handle,"createPlugin");
+	if (dlerror()) {
+		DBGA("Could not load symbol createPlugin from library " << filename.toAscii().constData());
+		return NULL;
+	}
 
-  //read the type of plugin
-  PluginCreator::GetTypeFctn getTypeFctn;
-  *(void **)(&getTypeFctn) = dlsym(handle,"getType");
-  if (dlerror()) {
-    DBGA("Could not load symbol getType from library " << filename.toAscii().constData());
-    return NULL;
-  }
-  std::cout << "Function name " << (*getTypeFctn)() <<std::endl;
-  std::string type = (*getTypeFctn)();
-  if (type.empty()) {
-    DBGA("Could not get plugin type from library " << filename.toAscii().constData());
-  }
+	//read the type of plugin
+	PluginCreator::GetTypeFctn getTypeFctn;
+	*(void **)(&getTypeFctn) = dlsym(handle,"getType");
+	if (dlerror()) {
+		DBGA("Could not load symbol getType from library " << filename.toAscii().constData());
+		return NULL;
+	}
+	std::cout << "Function name " << (*getTypeFctn)() <<std::endl;
+	std::string type = (*getTypeFctn)();
+	if (type.empty()) {
+		DBGA("Could not get plugin type from library " << filename.toAscii().constData());
+	}
 
-  //create the PluginCreator and push it back
-  bool autoStart = true;
-  std::string defaultArgs;
-  PluginCreator *creator = new PluginCreator(handle, createPluginFctn, autoStart, type, defaultArgs);
-  return creator;
+	//create the PluginCreator and push it back
+	bool autoStart = true;
+	std::string defaultArgs;
+	PluginCreator *creator = new PluginCreator(handle, createPluginFctn, autoStart, type, defaultArgs);
+	return creator;
 }
